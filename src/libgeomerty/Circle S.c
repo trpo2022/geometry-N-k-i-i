70557017b6159e0c1fe4e557ada9832a4530bf41
Nikita Plotnikov <N-k-i-i@users.noreply.github.com>
2022-03-15 15:58:33 +0700

    Create lib function finding the area of a circle

---
+++ src/libgeomerty/Circle S.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "geometry.h"
+
+float SC(int r) {
+  float Sc;
+  Sc = 3.14 * pow(r, 2);
+  return Sc;
+}
---
+++ src/libgeomerty/geometry.h
@@ -0,0 +1,6 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+float SC(int r);
+
+#endif
